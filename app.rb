069368b6b6176563dbec8098d1bfaa3ec54b9450
huydo <doxuanhuy@gmail.com>
2013-04-20 09:31:53 +0900

    add random color for user

--- app.rb
+++ app.rb
@@ -15,10 +15,40 @@ class IrcLog
     return @db.execute("select date, user, content from #{@table} order by date(date) desc")
   end
     
-  def parse_row(row)
+  def simple_format(row)
     didx, uidx, cidx = @INDEXMAP["date"], @INDEXMAP["user"], @INDEXMAP["content"]
-    return {:date=>row[didx], :user=>row[uidx], :content=>row[cidx]} 
+    #format date 
+    date = DateTime.parse(row[didx]).strftime("posted:[%Y %m %d %T]")
+    
+    return {:date=>date, :user=>row[uidx], :content=>row[cidx]} 
   end  
+  
+  def color_format(rows)
+    didx, uidx, cidx = @INDEXMAP["date"], @INDEXMAP["user"], @INDEXMAP["content"]
+    uniq_usr = rows.map{ |r| r[uidx]}.uniq
+    color_hash_table = {}
+    idx = 0
+    uniq_usr.each do |u|
+      u_integer = 0
+      
+      r = rand(255).to_s(16)
+      g = rand(255).to_s(16)
+      b = rand(255).to_s(16)
+
+      r, b, g = [r, b, g].map { |s| if s.size == 1 then '0' + s else s end }      
+
+      color_hash_table[u] = r + g + b
+      idx += 1
+    end 
+
+    return rows.map do |r| 
+      { 
+        :date=>DateTime.parse(r[didx]).strftime("posted:[%Y %m %d %T]"), 
+        :user=>{:name=>r[uidx], :color=>color_hash_table[r[uidx]]}, 
+        :content=>r[cidx]
+      }
+    end 
+  end
 end
 
 get '/irc' do 
@@ -26,7 +56,7 @@ get '/irc' do
   @rows = []
   irc = IrcLog.new("irclog.db", "chatlog")
   _rows = irc.get_rows()
-  _rows.each { |r| @rows.push (irc.parse_row(r)) }
+  @rows = irc.color_format(_rows)
 
   erb :home
 end
