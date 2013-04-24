982dfa4a7c0dbaba0325ca79ad951c656e52ce8e
dxhuy1988 <doxuanhuy@gmail.com>
2013-04-24 15:10:47 +0900

    fix some bug

--- rubybot/bot.rb
+++ rubybot/bot.rb
@@ -1,8 +1,23 @@
 require 'cinch'
 require 'sqlite3'
+require 'ruby-debug'
 
 server = "irc.freenode.org"
 channel = "#huydxcinch"
+
+module BmConfig
+  @dbname = ENV['CINCH_DBNAME']
+  @dblocation = File.expand_path("~/Dropbox/db/")
+
+  def self.dbname
+    @dbname
+  end
+
+  def self.dblocation
+    @dblocation
+  end 
+end
+
 
 class SqlHelper
   def initialize options={}
@@ -13,8 +28,20 @@ end
 
 
 class BookmarkSqlHelper < SqlHelper
-  def add_bookmark(url, description, tags=[])
-
+  def add_bookmark(usr, url, tags=[])
+    tag_ids = []
+    unless tags.empty? 
+      tags.each {|t| @db.execute ("insert into tag name values(#{t})") }
+      tag_ids.push (@db.execute "select last_insert_rowid()")
+    end
+    @db.execute ("insert into bookmark (url, user_name, time_created) (#{url}, #{usr}, datetime(now))")
+    bm_id = @db.execute "select last_insert_rowid()"
+    
+    unless tag_ids.empty?
+      tag_ids.each { |tid| 
+        @db.execute ("insert into tagmap (bookmark_id, tag_id) values(#{bm_id}, #{tid})") 
+      } 
+    end
   end
 
   def get_bookmark_by_tag(tags=[])
@@ -28,19 +55,19 @@ end
 
 class TaskSqlHelper < SqlHelper
   def get_task(usr)
-
   end
 end
 
 class TaskPlugin 
   include Cinch::Plugin
+  include BmConfig
 
   timer 60, method: :check_tasks
 
   def initialize(*args)
     super
-    dbname = ENV['CINCH_DBNAME']
-    dblocation = File.expand_path("~/Dropbox/db/")   
+    dbname = BmConfig::dbname
+    dblocation = BmConfig::dblocation   
     @sql_helper = BookmarkSqlHelper.new({:db=>(dblocation+"/"+dbname)})
   end 
 
@@ -50,22 +77,42 @@ class TaskPlugin
 end
 
 bot = Cinch::Bot.new do
+  class << self
+    def sql_helper
+      dbname = BmConfig::dbname
+      dblocation = BmConfig::dblocation   
+      BookmarkSqlHelper.new({:db=>(dblocation+"/"+dbname)})
+    end
+  end
+
+  bm_add_pattern = /^bm add (.*)/
+  bm_delete_pattern = /^bm delete (.*)/
+  bm_show_pattern = /^bm show (.*)/
+
   configure do |c|
     c.server = server
     c.channels = [channel]
     c.plugins.plugins = [TaskPlugin]
   end
 
-  on :message, /^bm add (.*)/ do |m|
-     m.reply "Hello, #{m.user.nick}"
+  on :message, bm_add_pattern do |m|
+    mes = m.params[1]
+    usr = m.user.nick
+
+    params = mes.scan(bm_add_pattern).join("").split
+    url = params[0]
+    tags = params[1..-1] 
+    
+    bot = self.bot
+    bot.sql_helper.add_bookmark(usr, url, tags)
   end
 
-  on :message, /^bm delete (.*)/ do |m|
-     m.reply "Hello, #{m.user.nick}"
+  on :message, bm_delete_pattern do |m|
+    m.reply "Hello, #{m.user.nick}"
   end
 
-  on :message, /^bm show (.*)/ do |m|
-     m.reply "Hello, #{m.user.nick}"
+  on :message, bm_show_pattern do |m|
+    m.reply "Hello, #{m.user.nick}"
   end
 end
 
